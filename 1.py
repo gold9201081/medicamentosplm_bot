9774f74662af55cc0e0cee696c6cd5aed052535a
gold9201081 <gold9201081@gmail.com>
2024-07-16 14:40:57 +0800

    second commit

--- 1.py
+++ 1.py
@@ -8,12 +8,39 @@ driver.get(url)
 print("Loading finished!")
 sleep(3)
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+data_set = []
 for letter in letters:
     url_1 = url.rsplit("/", 2)[0] + "/" + letter + "/1"
     print(url_1)
     driver.get(url_1)
     sleep(1)
-    elements = driver.find_elements(By.ID, "editable")
-    for element in elements:
-        element.click()
+    links = []
+    lis = driver.find_elements(By.ID, "editable")
+    for li in lis:
+        links.append(li.find_element(By.TAG_NAME, "a").get_attribute('href'))
+    print(links)
+    for link in links:
+        print("AAAAAAAA")
+        print(link)
+        sleep(2)
+        # link_url = link.find_element(By.TAG_NAME, "a").get_attribute('href')
+        # print(link_url)
+        # sleep(2)
+        driver.get(link)
+        sleep(2)
+        filas = driver.find_elements(By.ID, "editable")
+        for fila in filas:
+            company = fila.find_element(By.ID, "prescription-division-1").text
+            medicine = fila.find_element(By.CLASS_NAME, "first").text
+            substance = fila.find_element(By.ID, "prescription-substance-1").text
+            pharmaceutical_form = fila.find_element(By.ID, "prescription-pharmaForm-1").text
+            presentation = fila.find_element(By.ID, "prescription-presentation-1").text
+            data = {"company": company, "medicine": medicine, "substance": substance, "pharmaceutical_form": pharmaceutical_form, "presentation": presentation}
+            data_set.append(data)
+        print(data_set)
+        driver.back()
+        # link_url = ""
+        print("BBBBBBBBB")
+        sleep(5)
+driver.quit()
     
